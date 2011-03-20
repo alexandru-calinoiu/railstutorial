dbfea12dd949c031e7d32015fe077870dcaabd0f
calin <calinuswork@gmail.com>
2011-03-20 21:54:55 +0200

    added valiation on content length

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Micropost do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "content" do
+    it "should be invalid if longer than 140 chars" do
+      micropost = Micropost.new(:content => (0...141).map { ('a'..'z').to_a[rand(26)] }.join)
+
+      micropost.should have(1).error_on(:content)
+    end
+  end
 end
